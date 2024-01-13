3f14b99beedf39af529ff983311ddb9223a33088
Won Sick <king258436@nave.com>
2024-01-13 17:57:00 +0900

    members 리다이렉트, 링크 구현

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -32,7 +32,7 @@ public class MemberController {
         //2.리파지터리로 엔티티를 DB에 저장.
         Member saved = memberRepository.save(member);
         log.info(saved.toString());
-        return"";
+        return"redirect:/members/"+saved.getId();
     }
 
     @GetMapping("/members/{id}")
