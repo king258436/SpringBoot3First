2cc8e888586af6a77d4251022d41cb5d932b462a
Won Sick <king258436@nave.com>
2024-01-15 00:25:27 +0900

    스프링부트 7장 셀프체크

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -54,4 +54,28 @@ public class MemberController {
         return "members/index";
     }
 
+    @GetMapping("/members/{id}/edit")
+    public String edit(@PathVariable Long id, Model model){
+        // 수정할 데이터 가져오기
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+        // 모델에 데이터 등록하기
+        model.addAttribute("member",memberEntity);
+        // 뷰 페이지 설정
+        return "members/edit";
+    }
+    @PostMapping("/members/update")
+    public String update(MemberForm form){
+        //1. DTO를 엔티티로 변경
+        Member memberEntity = form.toEntity();
+        //2. 엔티티를 DB에 저장
+        // 2-1 DB에서 기존 데이터 가져오기
+        Member target = memberRepository.findById(memberEntity.getId()).orElse(null);
+        // 2-2 기존 데이터 값을 갱신하기
+        if(target!=null){
+            memberRepository.save(memberEntity);
+        }
+        //3. 수정 결과 페이지로 리다이렉트
+        return "redirect:/members/" + memberEntity.getId();
+    }
+
 }
