60e02f0deb2bd81c76cd3662e0158d3dec23fd99
Won Sick <king258436@nave.com>
2024-01-15 23:01:11 +0900

    members delete 구현

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 
@@ -76,6 +77,19 @@ public class MemberController {
         }
         //3. 수정 결과 페이지로 리다이렉트
         return "redirect:/members/" + memberEntity.getId();
+    }
+
+    @GetMapping("/members/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr){
+        //1. 삭제할 대상 가저오기
+        Member target = memberRepository.findById(id).orElse(null);
+        //2. 대상 엔티티 삭제하기
+        if(target!=null){
+            memberRepository.delete(target);
+        }
+        rttr.addFlashAttribute("msg","삭제됐습니다.");
+        //3. 결과 페이지로 리다이렉트
+        return "redirect:/members";
     }
 
 }
