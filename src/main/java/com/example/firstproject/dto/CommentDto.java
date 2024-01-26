8480286c965d919f8a81dc971ab0ec94d2e49722
WonsickChoi <king258436@naver.com>
2024-01-26 17:46:11 +0900

    15 .1~.3 댓글 조회하기 (Rest Controller)

---
+++ src/main/java/com/example/firstproject/dto/CommentDto.java
@@ -0,0 +1,27 @@
+package com.example.firstproject.dto;
+
+import com.example.firstproject.entity.Comment;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+@NoArgsConstructor
+@Getter
+public class CommentDto {
+    private Long id;
+    private Long articleId;
+    private String nickname;
+    private String body;
+
+    public static CommentDto createCommentDto(Comment comment) {// 생성메소드임 그래서 클래스 이름 다음에 메서드이름나옴
+        return new CommentDto(
+                comment.getId(),
+                comment.getArticle().getId(),
+                comment.getNickname(),
+                comment.getBody()
+        );
+    }
+}
